b5e65ca1cba903f99592d820616d1744c8081911
pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>
2023-02-16 16:38:07 +0100

    Update infinite_potential_well.py
    defined sections of the programme
    
    Signed-off-by: pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>

--- infinite_potential_well.py
+++ infinite_potential_well.py
@@ -1,1 +1,23 @@
-#progetto per calcolare e plottare autovalori e autostati di un eletrone in una buca di potenziale infinita e larga L
+#progetto per calcolare e plottare autovalori e autostati di un elettrone in una buca di potenziale infinita e larga L
+#import
+import numpy as np
+import matplotlib  
+import scipy as sp
+
+
+#parameters electron_mass, hbar, elementary_charge
+
+
+
+#input
+
+
+
+#definition of functions, to be put in sparate files py
+
+
+
+#calculations
+
+
+#plot
