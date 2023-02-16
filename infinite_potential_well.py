30019b5a22f4302bc32104fd76bbbcec004c7880
pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>
2023-02-16 18:04:37 +0100

    Update infinite_potential_well.py
    program to be continued, L to be converted, E_1 to be decided
    
    Signed-off-by: pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>

--- infinite_potential_well.py
+++ infinite_potential_well.py
@@ -6,20 +6,31 @@ import scipy as sp
 
 
 #parameters electron_mass, hbar, elementary_charge
-
-
+hbar = constants.hbar
+m = constants.m_e
+pi = constants.pi
 
 #input
 print('This programme gives the first n eigenfunctions and eigenvalues of an electron in a infinite potential well with a width L.') 
-L=float(input('Please, insert the width of the potential well in Angstrom: L = '))
+width =float(input('Please, insert the width of the potential well in Angstrom: L = '))
 n=int(input('Please, insert the number of the first eigenvalues and eigenstates of the electron you want to know: n = ')) 
 
+#x?????
+#L=width*                             #convertire unita' di misura
 
-#definition of functions, to be put in sparate files py
+#definition of functions, to be put in separate files py
+def eigenfunction(L,n):
+  psi_n=sqrt(2/L)*cos(n*pi*x/L)
+  return(psi_n)
 
-
+def eigenvalues(L,n):
+  E_1=hbar**2pi**2/(2mL**2)   #meglio definirla fuori dalla funzione? altrimenti a ogni n la ricalcola inutilmente
+  E_n=n**2*E_1                            # 
+  return(E_n)
 
 #calculations
-
+for level in range (n):
+  
+  
 
 #plot
