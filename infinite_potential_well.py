d1b659d3818ed73afc8e2d715ccdd09733243061
pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>
2023-02-16 12:48:07 +0100

    Update and rename potential_well.py to infinite_potential_well.py
    updated description and title
    
    Signed-off-by: pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>

---
+++ infinite_potential_well.py
@@ -0,0 +1,1 @@
+#progetto per calcolare e plottare autovalori e autostati di un eletrone in una buca di potenziale infinita e larga L
--- potential_well.py
+++
@@ -1,3 +0,0 @@
-#progetto per calcolare autovalori e autostati di una buca di potenziale larga L e infinita
-def somma(x,y):
-    return(x+y)
