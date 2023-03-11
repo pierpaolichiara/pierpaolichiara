746d7d97a248ce42bee763dd5132ff4a049ac765
pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>
2023-03-11 12:00:42 +0100

    Update infinite_potential_well.py
    Started calculations
    
    Signed-off-by: pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>

--- infinite_potential_well.py
+++ infinite_potential_well.py
@@ -10,9 +10,9 @@ hbar = constants.hbar
 m = constants.m_e
 pi = constants.pi
 
-#input
+#input n, L
 print('This programme gives the first n eigenfunctions and eigenvalues of an electron in a infinite potential well with a width L.') 
-width =float(input('Please, insert the width of the potential well in Angstrom: L = '))
+width =float(input('Please, insert the width of the potential well in Angstrom: L = '))           
 n=int(input('Please, insert the number of the first eigenvalues and eigenstates of the electron you want to know: n = ')) 
 
 #x?????
@@ -23,14 +23,14 @@ def eigenfunction(L,n):
   psi_n=sqrt(2/L)*cos(n*pi*x/L)
   return(psi_n)
 
-def eigenvalues(L,n):
+def eigenvalues(L,n):         #definisco direttamente  E_n senza E_1?
   E_1=hbar**2pi**2/(2mL**2)   #meglio definirla fuori dalla funzione? altrimenti a ogni n la ricalcola inutilmente
   E_n=n**2*E_1                            # 
   return(E_n)
 
 #calculations
 for level in range (n):
-  
+  print(level, eigenfunction(L,n), eigenvalues(L,n)
   
 
 #plot
