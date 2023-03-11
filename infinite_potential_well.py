1b3fccfbf380a38f81f2e0ef05e7b34e111244b5
pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>
2023-03-11 13:03:23 +0100

    Update infinite_potential_well.py
    def L
    correction eigenvalues
    correction calculation n -> level+1
    
    Signed-off-by: pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>

--- infinite_potential_well.py
+++ infinite_potential_well.py
@@ -8,7 +8,7 @@ import scipy as sp
 #parameters electron_mass, hbar, elementary_charge
 hbar = constants.hbar
 m = constants.m_e
-pi = constants.pi
+#pi = constants.pi
 
 #input n, L
 print('This programme gives the first n eigenfunctions and eigenvalues of an electron in a infinite potential well with a width L.') 
@@ -16,7 +16,7 @@ width =float(input('Please, insert the width of the potential well in Angstrom: 
 n=int(input('Please, insert the number of the first eigenvalues and eigenstates of the electron you want to know: n = ')) 
 
 #x?????
-#L=width*                             #convertire unita' di misura
+L=width                             #convertire unita' di misura
 
 #definition of functions, to be put in separate files py
 def eigenfunction(L,n):
@@ -24,13 +24,13 @@ def eigenfunction(L,n):
   return(psi_n)
 
 def eigenvalues(L,n):         #definisco direttamente  E_n senza E_1?
-  E_1=hbar**2pi**2/(2mL**2)   #meglio definirla fuori dalla funzione? altrimenti a ogni n la ricalcola inutilmente
+  E_1=hbar**2*pi**2/(2*m*L**2)   #meglio definirla fuori dalla funzione? altrimenti a ogni n la ricalcola inutilmente
   E_n=n**2*E_1                            # 
   return(E_n)
 
 #calculations
 for level in range (n):
-  print(level, eigenfunction(L,n), eigenvalues(L,n)
+  print(level+1, eigenfunction(L,level+1), eigenvalues(L,level+1))
   
 
 #plot
