2f711d2a16b9642210d6e885fa218a2197124398
pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>
2023-03-22 18:13:12 +0100

    corrected ax plot
    Signed-off-by: pierpaolichiara <123557338+pierpaolichiara@users.noreply.github.com>

--- infinite_potential_well.py
+++ infinite_potential_well.py
@@ -40,7 +40,7 @@ fig, ax = plt.subplots()
 for level in range (1, n+1):
     print(level, eigenvalues(L,level)/eigenvalues(L,1))
     y_array = [eigenfunction(L,level ,x) + 0.1*eigenvalues(L,level)/eigenvalues(L,1) for x in x_array]
-ax.plot(x_array, y_array, label =\
+ax.plot(x_array, y_array)
 ax.vlines(x=-L/2, ymin=0, ymax=1)
 fig.legend()
 
